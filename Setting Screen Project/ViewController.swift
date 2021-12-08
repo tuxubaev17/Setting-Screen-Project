9821daa113e1202ebbba5c0df13b1e4fc58f303a
Alikhan <tuxubaev17@gmail.com>
2021-12-08 10:54:46 +0500

    Initial Commit

---
+++ Setting Screen Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Setting Screen Project
+//
+//  Created by Alikhan Tuxubayev on 08.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
