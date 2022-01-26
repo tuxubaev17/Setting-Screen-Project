453c8544fd0b195981c8e1acca78165702e11eb1
Alikhan <tuxubaev17@gmail.com>
2022-01-26 14:15:38 +0600

    Create model

--- Setting Screen Project/Sources/Scenes/Model/SettingSectionModel.swift
+++ Setting Screen Project/Sources/Scenes/Model/SettingSectionModel.swift
@@ -7,27 +7,6 @@
 
 import UIKit
 
-struct Section {
-    let options: [SettingsOption]
-}
-
-struct SettingsOption {
-    let title: String
-    let icon: UIImage?
-    let iconBackgroundColor: UIColor
-    let mode: String?
-    var isOn: Bool? = nil
-    var notificationBackgroundColor: UIColor? = nil
-    var notificationNum: String? = nil
-    var typeCell: SettingsType
-}
-
-enum SettingsType {
-    case staticCell
-    case switchCell
-    case notificationCell
-}
-
 extension Section {
     static func getSettingCell() -> [Section] {
         return [
